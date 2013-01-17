45276444a6cd5e13da622a677b65a005b91c083c
Zarino Zappia <mail@zarino.co.uk>
2013-01-17 12:19:05 +0000

    handle url hash errors

--- js/download.js
+++ js/download.js
@@ -14,10 +14,28 @@ function readSettings(success, error){
     return success(settings)
 }
 
+function showAlert(title, message, level){
+    // [title] and [message] should be html strings. The first is displayed in bold.
+    // If [level] is a truthful value, the alert is printed in red.
+    level = level || 0;
+    var $div = $('<div>').addClass('alert').html('<button type="button" class="close" data-dismiss="alert">×</button>');
+    $div.append('<strong>' + title +'</strong> ' + message)
+    if(level){
+        $div.addClass('alert-error');
+    }
+    $div.prependTo('body');
+}
+
 $(function(){
     readSettings(function(settings){
         console.log(settings)
     }, function(error){
-        console.warn(error)
+        if(error=='window.location.hash not supplied'){
+            showAlert('Which dataset do you want to visualise?', 'You didn&rsquo;t supply a JSON object of settings in the URL hash. Are you sure you followed the right link?');
+        } else if(error=='window.location.hash is invalid JSON'){
+            showAlert('Could not read settings from URL hash!', 'The settings supplied in your URL hash are not a valid JSON object. Are you sure you followed the right link?');
+        } else {
+            showAlert('Oh noes!', 'Something mysterious went wrong when we tried to load your dataset settings. Are you sure you followed the right link?')
+        }
     })
 })
