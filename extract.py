96ce9ac2a9acf11f0a2dff8231df7adcbdad28ad
Zarino Zappia <mail@zarino.co.uk>
2013-01-17 17:10:48 +0000

    extract.py now takes a box url as a command line argument

--- extract.py
+++ extract.py
@@ -1,39 +1,51 @@
 #!/usr/bin/env python
 
 import requests
+import optparse
 from openpyxl.workbook import Workbook
 from openpyxl.writer.excel import ExcelWriter
 
 debug = False
 
+usage = """Convert data from a ScraperWiki box into an Excel spreadsheet.
+Takes one argument: the full URL of the target box.
+Example: ./extract.py http://box.scraperwiki.com/mybox/publish_token"""
+
 def sqlite(dataset_box_url, q):
     if debug: print 'GET %s/sqlite?q=%s' % (dataset_box_url, q)
     return requests.get(dataset_box_url + '/sqlite', params={'q': q})
 
-def extract(dataset_box_url):
-    r = sqlite(dataset_box_url, 'select name from sqlite_master where type="table"')
-    tables = [ x['name'] for x in r.json ]
+def extract():
+    parser = optparse.OptionParser(usage=usage)
+    (options, args) = parser.parse_args()
 
-    wb = Workbook()
-    wb.remove_sheet(wb.get_active_sheet())
+    if len(args):
+        dataset_box_url = args[0]
+        r = sqlite(dataset_box_url, 'select name from sqlite_master where type="table"')
+        tables = [ x['name'] for x in r.json ]
 
-    for table in tables:
-        if debug: print table
-        ws = wb.create_sheet(title=table)
-        r = sqlite(dataset_box_url, 'select * from [%s]' % table)
-        data = r.json
-        ws.append(data[0].keys())
-        for row in data:
-            ws.append(row.values())
-        ws.cell(row=1, column=1).value = table
+        wb = Workbook()
+        wb.remove_sheet(wb.get_active_sheet())
 
-    wb.save(filename='http/spreadsheet.xlsx')
-    if not debug:
-        print '["spreadsheet.xlsx"]'
+        for table in tables:
+            if debug: print table
+            ws = wb.create_sheet(title=table)
+            r = sqlite(dataset_box_url, 'select * from [%s]' % table)
+            data = r.json
+            ws.append(data[0].keys())
+            for row in data:
+                ws.append(row.values())
+            ws.cell(row=1, column=1).value = table
 
+        wb.save(filename='http/spreadsheet.xlsx')
+        if not debug:
+            print '["spreadsheet.xlsx"]'
+
+    else:
+        print usage
 
 try:
-    extract('https://box.scraperwiki.com/zarino.lastfm/f2cf672ac2b8405')
+    extract()
 except Exception as e:
     print '"We encountered a Python error while extracting your dataset: %s"' % e
     if debug: raise
