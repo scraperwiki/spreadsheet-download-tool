b65c636415874efe6c647526744c7bf16e46dca9
Zarino Zappia <mail@zarino.co.uk>
2013-10-24 17:49:02 +0100

    major overhaul - new _state table schema, new extraction file which (for now) only does CSV output

---
+++ create_downloads.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests  # requires Python requests 1.x
+import xlwt
+import unicodecsv
+import traceback # for formatting exceptions
+import json # for decoding API responses
+import collections # for parsing JSON as ordereddicts
+import lxml.html # for parsing "grid" HTML tables
+from tempfile import mkstemp
+from datetime import datetime
+import os
+from os.path import join, abspath, dirname
+import scraperwiki
+
+
+PAGE_SIZE = 5000  # how many rows to request from the SQL API at any one time
+
+
+class CsvOutput(object):
+    def __init__(self):
+        self.tempfiles = {}
+        self.writers = {}
+
+    def add_table(self, table_name, column_names):
+        self.tempfiles[table_name] = make_temp_file('.csv')
+        f = open(self.tempfiles[table_name], 'w')
+        self.writers[table_name] = unicodecsv.DictWriter(f, column_names)
+        self.writers[table_name].writeheader()
+
+    def add_grid(self, grid_name):
+        self.tempfiles[grid_name] = make_temp_file('.csv')
+        f = open(self.tempfiles[grid_name], 'w')
+        self.writers[grid_name] = unicodecsv.writer(f, encoding='utf-8')
+
+    def write_rows(self, table_name, rows):
+        self.writers[table_name].writerows(rows)
+
+    def finalise_file(self, table_name):
+        replace_tempfile(self.tempfiles[table_name], "http/{}.csv".format(table_name))
+        del self.tempfiles[table_name]
+        del self.writers[table_name]
+
+    # not used any more
+    def finalise_all(self):
+        for table_name, tempfile in self.tempfiles.items():
+            replace_tempfile(tempfile, "http/{}.csv".format(table_name))
+        self.tempfiles = {}
+        self.writers = {}
+
+
+def main():
+    box_url = get_box_url()
+    tables = get_dataset_tables(box_url)
+    grids = get_dataset_grids(box_url)
+
+    csv_outputter = CsvOutput()
+
+    for table in tables:
+        save_state('{}.csv'.format(table['name']), 'table', table['name'], 'generating')
+        csv_outputter.add_table(table['name'], table['columns'])
+        for some_rows in get_paged_rows(box_url, table['name']):
+            csv_outputter.write_rows(table['name'], some_rows)
+        csv_outputter.finalise_file(table['name'])
+        save_state("{}.csv".format(table['name']), 'table', table['name'], 'generated')
+
+    for grid in grids:
+        save_state('{}.csv'.format(grid['name']), 'grid', grid['name'], 'generating')
+        csv_outputter.add_grid(grid['name'])
+        csv_outputter.write_rows(grid['name'], get_grid_rows(grid['url']))
+        csv_outputter.finalise_file(grid['name'])
+        save_state("{}.csv".format(grid['name']), 'grid', grid['name'], 'generated')
+
+
+def get_dataset_tables(box_url):
+    tables = []
+    database_meta = get_database_meta(box_url)
+    for table_name, table_meta in database_meta['table'].items():
+        if not table_name.startswith('_'):
+            tables.append({
+                'id': table_name,
+                'name': table_name,
+                'columns': table_meta['columnNames']
+            })
+    return tables
+
+
+def get_dataset_grids(box_url):
+    grids = []
+    try:
+        results = query_sql_database(box_url, 'SELECT * FROM _grids')
+        for result in results:
+            grids.append({
+                'id': result['checksum'],
+                'name': result['title'],
+                'url': result['url']
+            })
+    except Exception as e:
+        log('could not get _grids:')
+        log(e)
+    return grids
+
+
+def log(string):
+    print string
+
+
+def get_box_url():
+    filename = abspath(join(dirname(__file__), '..', 'dataset_url.txt'))
+    try:
+        with open(filename, 'r') as f:
+            return f.read().strip()
+    except IOError:
+        raise RuntimeError("ERROR: No dataset URL in {}".format(filename))
+
+
+def call_api(box_url, params=None):
+    # returns sql api output as a Python dict/list
+    response = requests.get(box_url, params=params)
+    log("GET %s" % response.url)
+    if response.status_code == requests.codes.ok:
+        return json.loads(response.content,
+                          object_pairs_hook=collections.OrderedDict)
+    else:
+        response.raise_for_status()
+
+
+def query_sql_database(box_url, query):
+    return call_api("%s/sql" % box_url, {"q": query})
+
+
+def get_database_meta(box_url):
+    return call_api("%s/sql/meta" % box_url)
+
+
+def get_paged_rows(box_url, table_name):
+    start = 0
+    while True:
+        q = 'SELECT * FROM "%s" LIMIT %d, %d' % (table_name, start, PAGE_SIZE)
+        rows = query_sql_database(box_url, q)
+        if not rows:
+            break
+        yield rows
+        start += PAGE_SIZE
+
+
+def get_grid_rows(grid_url):
+    response = requests.get(grid_url)
+    log("GET %s" % response.url)
+    dom = lxml.html.fromstring(response.text)
+    table = dom.cssselect('table')[0]
+
+    data = []
+    for tr in table.cssselect('tr'):
+        row = []
+        for td in tr.cssselect('td'):
+            row.append(td.text_content())
+        data.append(row)
+    return data
+
+
+def save_state(filename, source_type, source_id, state):
+    log("%s %s" % (filename, state))
+    if state == 'generating':
+        scraperwiki.sql.save(['filename'], {
+            'filename': filename,
+            'state': 'generating',
+            'created': None,
+            'source_type': source_type,
+            'source_id': source_id
+        }, '_state')
+    elif state == 'generated':
+        scraperwiki.sql.save(['filename'], {
+            'filename': filename,
+            'state': 'generated',
+            'created': '{}+00:00'.format(datetime.now().isoformat()),
+            'source_type': source_type,
+            'source_id': source_id
+        }, '_state')
+    elif state == 'failed':
+        scraperwiki.sql.execute('delete from _state where filename = ?', filename)
+        scraperwiki.sql.commit()
+    else:
+        raise Exception("Unknown status: %s" % state)
+
+
+def make_temp_file(suffix):
+    (_, filename) = mkstemp(suffix=suffix, dir='http')
+    os.chmod(filename, 0644)  # world-readable
+    return filename
+
+
+def replace_tempfile(tmp, destination):
+    os.rename(tmp, destination)
+
+
+try:
+    main()
+except Exception as e:
+    print('Error while extracting your dataset: %s' % e)
+    scraperwiki.sql.save(
+        unique_keys=['message'],
+        data={'message': traceback.format_exc()},
+        table_name='_error')
+    raise
+
+
+
+
+
--- http/code.js
+++ http/code.js
@@ -1,4 +1,10 @@
-var issueTracker = 'https://github.com/scraperwiki/spreadsheet-download-tool/issues'
+window.timer = null
+window.tablesAndGrids = {
+  "tables": [],
+  "grids": []
+}
+window.files = []
+window.issueTracker = 'https://github.com/scraperwiki/spreadsheet-download-tool/issues'
 
 var reportAjaxError = function(jqXHR, textStatus, errorThrown, source){
   console.log(source + ' returned an ajax error:', jqXHR, textStatus, errorThrown)
@@ -9,54 +15,99 @@ var getDatasetTablesAndGrids = function(cb){
   // calls the `cb` callback with an object containing
   // lists of tables and grids in the parent dataset
   // eg: {"tables": [{"id":"_grids", "name":"_grids"}], "grids": [...]}
-  var tablesAndGrids = {
-    "tables": [],
-    "grids": []
-  }
   scraperwiki.dataset.sql.meta().fail(function(jqXHR, textStatus, errorThrown){
     reportAjaxError(jqXHR, textStatus, errorThrown, 'scraperwiki.dataset.sql.meta()')
-    cb(tablesAndGrids)
+    cb(window.tablesAndGrids)
   }).done(function(meta){
-    console.log('dataset contains', meta.table.length, 'tables:', meta.table)
     if(meta.table.length == 0){
       scraperwiki.alert('Your dataset has no tables', 'This shouldn&rsquo;t really be an error. We should handle this more gracefully.')
-      cb(tablesAndGrids)
+      cb(window.tablesAndGrids)
     } else {
       $.each(meta.table, function(table_name, table_meta){
-        tablesAndGrids.tables.push({"id": table_name, "name": table_name})
+        if(table_name.indexOf('_') != 0){
+          window.tablesAndGrids.tables.push({"id": table_name, "name": table_name})
+        }
       })
       if('_grids' in meta.table){
         scraperwiki.dataset.sql('SELECT * FROM _grids').fail(function(jqXHR, textStatus, errorThrown){
           reportAjaxError(jqXHR, textStatus, errorThrown, 'scraperwiki.dataset.sql()')
-          cb(tablesAndGrids)
+          cb(window.tablesAndGrids)
         }).done(function(grids){
           $.each(grids, function(i, grid){
-            tablesAndGrids.grids.push({"id": grid.checksum, "name": grid.title})
+            window.tablesAndGrids.grids.push({"id": grid.checksum, "name": grid.title})
           })
-          cb(tablesAndGrids)
+          cb(window.tablesAndGrids)
         })
       }
     }
   })
 }
 
-var renderListItem = function(id, filename, state, timestamp){
-  // `id` should be a unique id for the table/grid
-  // `name` should be a filename (either generated, or prospective)
-  // `state` should be either "generated", "generating" or "waiting"
-  // `timestamp` should (optionally) be an ISO-8601 creation date for the file
-  var $li = $('<li>')
-  $li.attr('data-id', id)
-  var $a = $('<a>')
-  $a.append('<span class="filename">' + filename + '</span>')
-  if(state == 'generated'){
-    $a.addClass(filename.split('.').pop()) // gets everything after the last dot (ie: extension)
-    if(typeof timestamp === 'string'){
-      $a.attr('data-timestamp', timestamp)
-      $a.append('<span class="state">' + moment(timestamp).add(1, 'hour').fromNow() + '</span>')
+var generateFileList = function(cb){
+  // given a load of sources in window.tablesAndGrids, and
+  // information from the _state SQL table, this function
+  // contructs a list of files generated / to be generated
+  $.each(window.tablesAndGrids.tables, function(i, table){
+    window.files.push({
+      'filename': table.name + '.csv',
+      'state': 'waiting',
+      'created': null,
+      'source_type': 'table',
+      'source_id': table.name
+    })
+  })
+  $.each(window.tablesAndGrids.grids, function(i, grid){
+    window.files.push({
+      'filename': grid.name + '.csv',
+      'state': 'waiting',
+      'created': null,
+      'source_type': 'table',
+      'source_id': grid.id
+    })
+  })
+  updateFileList(cb)
+}
+
+var updateFileList = function(cb){
+  scraperwiki.tool.sql('SELECT filename, state, created FROM _state').done(function(files){
+    $.each(files, function(i, file){
+      fileRecordToUpdate = _.findWhere(window.files, {'filename':file.filename})
+      if(typeof fileRecordToUpdate !== 'undefined'){
+        fileRecordToUpdate.state = file.state
+        fileRecordToUpdate.created = file.created
+      }
+    })
+    cb()
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    if(/database file does not exist/.test(jqXHR.responseText)){
+      console.log('first run!')
+      regenerate()
+    } else {
+      console.log('updateFileList failed', jqXHR, textStatus, errorThrown)
     }
-    $a.attr('href', scraperwiki.readSettings().target.url + '/http/' + filename)
-  } else if(state == 'generating'){
+    cb()
+  })
+}
+
+var renderListItem = function(file){
+  // `file.source_id` should be a unique id for the table/grid
+  // `file.source_type` should be either "table" or "grid"
+  // `file.filename` should be a filename (either generated, or prospective)
+  // `file.state` should be either "generated", "generating" or "waiting"
+  // `file.created` should (optionally) be an ISO-8601 creation date for the file
+  var $li = $('<li>')
+  $li.attr('data-source-id', file.source_id)
+  $li.attr('data-source-type', file.source_type)
+  var $a = $('<a>')
+  $a.append('<span class="filename">' + file.filename + '</span>')
+  if(file.state == 'generated'){
+    $a.addClass(file.filename.split('.').pop()) // gets everything after the last dot (ie: extension)
+    if(typeof file.created === 'string'){
+      $a.attr('data-timestamp', file.created)
+      $a.append('<span class="state">' + moment(file.created).fromNow() + '</span>')
+    }
+    $a.attr('href', scraperwiki.readSettings().target.url + '/http/' + file.filename)
+  } else if(file.state == 'generating'){
     $a.addClass('generating')
     $a.append('<span class="state">Generating</span>')
   } else {
@@ -64,29 +115,58 @@ var renderListItem = function(id, filename, state, timestamp){
     $a.append('<span class="state">Waiting</span>')
   }
   $li.append($a)
-  if($('li[data-id="' + id + '"]').length){
+  if($('li[data-source-id="' + file.source_id + '"]').length){
     // a list item for this file already exists, so replace it
-    $('li[data-id="' + id + '"]').replaceWith($li)
+    $('li[data-source-id="' + file.source_id + '"]').replaceWith($li)
   } else {
     // this is a new file, so append it to the list
     $('#files').append($li)
   }
 }
 
+var renderFiles = function(){
+  $.each(window.files, function(i, file){
+    renderListItem(file)
+  })
+  // $('p.controls').show()
+}
+
+var saveDatasetUrl = function(cb){
+  scraperwiki.tool.exec('echo "' + scraperwiki.readSettings().target.url + '" > ~/dataset_url.txt')
+  if(typeof cb != 'undefined'){
+    cb()
+  }
+}
+
+var regenerate = function(){
+  scraperwiki.tool.exec('echo "started"; run-one tool/create_downloads.py &> log.txt &')
+  window.timer = setInterval(check_status, 2000)
+}
+
+var check_status = function(){
+  var unfinishedFiles = _.reject(window.files, function(file){
+    return file.state == 'generated'
+  })
+  if(unfinishedFiles.length){
+    // some files are still outstanding, so check _state database for updates
+    updateFileList(renderFiles)
+  } else {
+    // no files outstanding, don't bother checking _state database
+    renderFiles()
+  }
+}
+
+
 $(function(){
 
-  getDatasetTablesAndGrids(function(tablesAndGrids){
-    $.each(tablesAndGrids.tables, function(i, table){
-      if(i==0){
-        renderListItem(table.id, table.name + '.csv', 'generated', moment().subtract('hour', 2).format())
-      } else if(i==1){
-        renderListItem(table.id, table.name + '.csv', 'generating')
-      } else {
-        renderListItem(table.id, table.name + '.csv', 'waiting')
-      }
+  saveDatasetUrl()
+
+  getDatasetTablesAndGrids(function(){
+    generateFileList(function(){
+      renderFiles()
     })
-    renderListItem('all_tables', 'all_tables.csv', 'waiting')
-    $('p.controls').show()
   })
+
+  $('#regenerate').on('click', regenerate)
 
 })
