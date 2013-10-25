f79b2b06579b514f2677df2b81ec14efc6a070a9
Zarino Zappia <mail@zarino.co.uk>
2013-10-25 10:16:48 +0100

    reintroduce all_tables.xls support

--- create_downloads.py
+++ create_downloads.py
@@ -50,27 +50,88 @@ class CsvOutput(object):
         self.writers = {}
 
 
+class ExcelOutput(object):
+    def __init__(self):
+        self.workbook = xlwt.Workbook(encoding="utf-8")
+        self.name = 'all_tables.xls'
+        self.sheets = {}
+        self.current_rows = {}
+        self.fail = False
+
+    def add_table(self, table_name, column_names):
+        self.sheets[table_name] = self.workbook.add_sheet(table_name)
+        for col_number, value in enumerate(column_names):
+            self.sheets[table_name].write(0, col_number, value)
+        self.current_rows[table_name] = 1
+
+    def add_grid(self, grid_name):
+        self.sheets[grid_name] = self.workbook.add_sheet(grid_name)
+        self.current_rows[grid_name] = 0
+
+    def write_rows(self, table_name, rows):
+        for row in rows:
+            self.write_row(table_name, row)
+
+    def write_row(self, table_name, row):
+        if self.fail:
+            return
+        if hasattr(row, "values") and callable(row.values):
+            row_values = row.values()
+        else:
+            row_values = row
+        for col_number, cell_value in enumerate(row_values):
+            try:
+                self.sheets[table_name].write(
+                    self.current_rows[table_name],
+                    col_number,
+                    cell_value)
+            except ValueError:
+                self.fail = True
+        self.current_rows[table_name] += 1
+
+    def finalise(self):
+        output_name = 'http/{}'.format(self.name)
+        if self.fail:
+            save_state(self.name, None, None, 'failed')
+            os.path.exists(output_name) and os.remove(output_name)
+            return
+        tempfile = make_temp_file('.xls')
+        self.workbook.save(tempfile)
+        replace_tempfile(tempfile, output_name)
+
+
 def main():
     box_url = get_box_url()
     tables = get_dataset_tables(box_url)
     grids = get_dataset_grids(box_url)
 
     csv_outputter = CsvOutput()
+    xls_outputter = ExcelOutput()
+
+    save_state('all_tables.xls', None, None, 'generating')
 
     for table in tables:
         save_state('{}.csv'.format(table['name']), 'table', table['name'], 'generating')
         csv_outputter.add_table(table['name'], table['columns'])
+        xls_outputter.add_table(table['name'], table['columns'])
         for some_rows in get_paged_rows(box_url, table['name']):
             csv_outputter.write_rows(table['name'], some_rows)
+            xls_outputter.write_rows(table['name'], some_rows)
         csv_outputter.finalise_file(table['name'])
         save_state("{}.csv".format(table['name']), 'table', table['name'], 'generated')
 
     for grid in grids:
         save_state('{}.csv'.format(grid['name']), 'grid', grid['name'], 'generating')
         csv_outputter.add_grid(grid['name'])
-        csv_outputter.write_rows(grid['name'], get_grid_rows(grid['url']))
+        xls_outputter.add_grid(grid['name'])
+        grid_rows = get_grid_rows(grid['url'])
+        csv_outputter.write_rows(grid['name'], grid_rows)
+        xls_outputter.write_rows(grid['name'], grid_rows)
         csv_outputter.finalise_file(grid['name'])
         save_state("{}.csv".format(grid['name']), 'grid', grid['name'], 'generated')
+
+    xls_outputter.finalise()
+    save_state('all_tables.xls', None, None, 'generated')
 
 
 def get_dataset_tables(box_url):
@@ -179,8 +240,13 @@ def save_state(filename, source_type, source_id, state):
             'source_id': source_id
         }, '_state')
     elif state == 'failed':
-        scraperwiki.sql.execute('delete from _state where filename = ?', filename)
-        scraperwiki.sql.commit()
+        scraperwiki.sql.save(['filename'], {
+            'filename': filename,
+            'state': 'failed',
+            'created': None,
+            'source_type': source_type,
+            'source_id': source_id
+        }, '_state')
     else:
         raise Exception("Unknown status: %s" % state)
 
